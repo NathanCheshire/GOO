40c6ac637368d873a26eb090b95e4a35a5c2ae38
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-03-11 15:05:20 -0600

    A royal bodge, found correct solutions for s1-s20 by randomly generating elements for x seconds

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,41 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.Semaphore;
 
 public class Main {
     /** finds the maximum order possible of an element from the symmetric group S sub n
      * @author Nathan Cheshire
      * @since 3-10-21
      */
-    private static ArrayList<ArrayList<Integer>> openCycles;
-    private static ArrayList<ArrayList<Integer>> closedCycles;
+    private static ArrayList<ArrayList<Integer>> cycles;
+    private static int currentN;
+    private static int secondsPerGOO = 2;
 
     public static void main(String[] args) {
-        //GOO(20); //420
+        GOO(20); //420
         GOO(5); //6
         GOO(7); //12
-        //GOO(13); //60
+        GOO(13); //60
     }
 
     private static void GOO(int n) {
         int max = 0;
+        currentN = n;
         ArrayList<Integer> using = new ArrayList<>();
-        openCycles = new ArrayList<>();
-        closedCycles = new ArrayList<>();
+        cycles = new ArrayList<>();
         ArrayList<Integer> productOfCycleLengths = new ArrayList<>();
 
         ArrayList<Integer> ones = new ArrayList<>();
         for (int i = 0 ; i < n ; i++) {
             ones.add(1);
         }
-
-        openCycles.add(ones);
+        Collections.sort(ones);
+        cycles.add(ones);
         ArrayList<Integer> nList = new ArrayList<>();
         nList.add(n);
-        openCycles.add(nList);
+        Collections.sort(nList);
+        cycles.add(nList);
 
         for (int i = n - 1 ; i > 0 ; i--) {
             ArrayList<Integer> cycle = new ArrayList<>();
@@ -44,28 +45,25 @@ public class Main {
                 cycle.add(1);
             }
 
-            if (!openCycles.contains(cycle))
-                openCycles.add(cycle);
+            Collections.sort(cycle);
+            cycles.add(cycle);
         }
 
         ArrayList<Integer> currentCycle;
+        long start = System.currentTimeMillis();
 
-        while (!openCycles.isEmpty()) {
-            currentCycle = new ArrayList<>(openCycles.get(0));
-
-            closedCycles.add(currentCycle);
-            openCycles.remove(currentCycle);
-            cycleGenerator(currentCycle);
+        while (System.currentTimeMillis() != start + secondsPerGOO * 1000) {
+            cycleGenerator(cycles.get(rand(cycles.size() - 1)));
         }
 
-        for (ArrayList<Integer> cycle : closedCycles) {
+        for (ArrayList<Integer> cycle : cycles) {
             productOfCycleLengths.add(lcmArray(cycle));
         }
 
         for (int i = 0 ; i < productOfCycleLengths.size() ;i++) {
             if (productOfCycleLengths.get(i) > max) {
                 max = productOfCycleLengths.get(i);
-                using = closedCycles.get(i);
+                using = cycles.get(i);
             }
         }
 
@@ -101,25 +99,30 @@ public class Main {
         else return gcd(b, a%b);
     }
 
-    private static int rand(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
+    private static int rand(int max) {
+        return (int) ((Math.random() * (max)) + 0);
     }
 
     //cycle given is already in cycles so don't change
     private static void cycleGenerator(ArrayList<Integer> cycle){
-        if (cycle.size() < 3) //len 2 right now so we don't do anything
+        if (cycle.size() < 3)
             return;
 
-        int r1 = rand(0, cycle.size() - 1);
-        int r2 = rand(0, cycle.size() - 1);
+        ArrayList<Integer> newCycle = new ArrayList<>(cycle);
+        int r1 = rand(newCycle.size() - 1);
+        int r2 = rand(newCycle.size() - 1);
+        int sum = newCycle.get(r1) + newCycle.get(r2);
+        newCycle.set(r1,sum);
+        newCycle.remove(r2);
+        Collections.sort(newCycle);
+        if (!cycles.contains(newCycle)) {
+            cycles.add(newCycle);
+        }
+    }
 
-        while (r2 == r1)
-             r2 = rand(0, cycle.size() - 1);
-
-        ArrayList<Integer> r1r2Copy = new ArrayList<>(cycle);
-        int sum = r1r2Copy.get(r1) + r1r2Copy.get(r2);
-        r1r2Copy.set(r1, sum);
-        r1r2Copy.remove(r2);
-        openCycles.add(r1r2Copy); //if not in here
+    private static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        else return n * factorial(n - 1);
     }
 }
