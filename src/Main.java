6c7403e955a4a5020c12b4f9bd94aa72a39cccb4
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-03-11 13:25:45 -0600

    Lots of bug fixes and logic updates, current problem: cycle generator is not generating all possible cycles

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,58 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 public class Main {
     /** finds the maximum order possible of an element from the symmetric group S sub n
      * @author Nathan Cheshire
      * @since 3-10-21
      */
+    private static ArrayList<ArrayList<Integer>> cycles;
+    private static ArrayList<ArrayList<Integer>> starterCycles;
+
     public static void main(String[] args) {
-        //GOO(20); //420
-        //GOO(40); //?
-        GOO(5);
-        //GOO(7); //12
-        //GOO(13); //60
+        GOO(20); //420
+        GOO(5); //6
+        GOO(7); //12
+        GOO(13); //60
     }
 
     private static void GOO(int n) {
         int max = 0;
         ArrayList<Integer> using = new ArrayList<>();
+        starterCycles = new ArrayList<>();
 
         ArrayList<Integer> productOfCycleLengths = new ArrayList<>(); //the product of the cycle lengths below
-        ArrayList<ArrayList<Integer>> cycles = new ArrayList<>(); //cycles of which all add up to n
-
-        long start = System.currentTimeMillis();
+        cycles = new ArrayList<>(); //cycles of which all add up to n
 
         ArrayList<Integer> ones = new ArrayList<>();
         for (int i = 0 ; i < n ; i++) {
             ones.add(1);
         }
 
-        cycles.add(ones);
+        starterCycles.add(ones);
+        ArrayList<Integer> nList = new ArrayList<>();
+        nList.add(n);
+        starterCycles.add(nList);
 
-        while (cycles.size() < factorial(n)) {
-            System.out.println(cycles.size());
-            ArrayList<Integer> randNew = new ArrayList<>(cycles.get(rand(0, cycles.size() - 1)));
+        for (int i = n - 1 ; i > 0 ; i--) {
+            ArrayList<Integer> cycle = new ArrayList<>();
+            cycle.add(i);
 
-            if (randNew.size() < 2)
-                continue;
-
-            int one = rand(0,randNew.size() - 1);
-            int two = rand(0,randNew.size() - 1);
-
-            while (one == two)
-                two = rand(0,randNew.size() - 1);
-
-            int addBackIn = randNew.get(one) + randNew.get(two);
-            randNew.remove(one);
-            randNew.remove(two);
-            randNew.add(addBackIn);
-
-            if (!cycles.contains(randNew)) {
-                cycles.add(randNew);
-                System.out.println("advanced");
+            for (int j = 0 ; j < n - i ; j++) {
+                cycle.add(1);
             }
+
+            if (!cycles.contains(cycle))
+                starterCycles.add(cycle);
         }
 
-        //start in middle of a random list from cycles
-        //between 1 and n - 1 times:
-        // remove two random elements, add the two elements together, add the result back in
-        // if this arraylist is not in cycles (!arrl.equals(arr2))
-        //      add it in then
+        for (ArrayList<Integer> cycle : starterCycles)
+            cycleGenerator(cycle);
 
         for (ArrayList<Integer> cycle : cycles) {
-            productOfCycleLengths.add(LCM(cycle));
+            productOfCycleLengths.add(lcmArray(cycle));
         }
 
         for (int i = 0 ; i < productOfCycleLengths.size() ;i++) {
@@ -73,17 +62,7 @@ public class Main {
             }
         }
 
-        long end = System.currentTimeMillis();
-        System.out.println("Found result in " + (end - start) + " ms.");
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0 ; i < using.size() ; i++)
-            if (i != using.size() - 1)
-                sb.append(using.get(i)).append(",");
-            else
-                sb.append(using.get(i));
-
-        System.out.println("max order of symmetric group S" + n + " is " + max + "\nusing: [" + sb.toString() + "]");
+        System.out.println("max order of symmetric group S" + n + " is " + max + " using: " + using.toString());
     }
 
     private static int[] convertIntegers(List<Integer> integers) {
@@ -96,45 +75,53 @@ public class Main {
         return ret;
     }
 
-    private static int LCM(ArrayList<Integer> arr) {
-        int min;
-        int max;
-        int x;
-        int lcm = 0;
-        int[] myArray = convertIntegers(arr);
-
-        for (int i = 0 ; i < myArray.length ; i++) {
-            for (int j = i + 1; j < myArray.length - 1 ; j++) {
-                if (myArray[i] > myArray[j]) {
-                    min = myArray[j];
-                    max = myArray[i];
-                }
-
-                else {
-                    min = myArray[i];
-                    max = myArray[j];
-                }
-
-                for (int k = 0 ; k < myArray.length ; k++) {
-                    x = k * max;
-
-                    if (x % min == 0) {
-                        lcm = x;
-                    }
-                }
-            }
-        }
-        return lcm;
+    private static int lcmArray(ArrayList<Integer> cycle) {
+        return lcmofarray(convertIntegers(cycle), 0, cycle.size());
     }
 
-    private static int factorial(int n) {
-        int fact = 1;
-        for (int i = 2; i <= n; i++)
-            fact = fact * i;
-        return fact;
+    private static int lcmofarray(int[] arr, int start, int end) {
+        if ((end-start)==1) return lcm(arr[start],arr[end-1]);
+        else return (lcm (arr[start], lcmofarray(arr, start+1, end)));
+    }
+
+    private static int lcm(int a, int b){
+        return ((a*b)/gcd(a,b));
+    }
+
+    private static int gcd(int a, int b){
+        if (a<b) return gcd(b,a);
+        if (a%b==0) return b;
+        else return gcd(b, a%b);
     }
 
     private static int rand(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    //cycle given is already in cycles so don't change
+    private static void cycleGenerator(ArrayList<Integer> cycle) {
+        if (cycle.size() < 3)
+            return;
+
+        int middleIndex = (int) Math.floor(cycle.size() / 2);
+        int middleValue = cycle.get(middleIndex);
+
+        if (middleIndex - 1 >= 0) {
+            int leftMidSum = middleValue + cycle.get(middleIndex - 1);
+            ArrayList<Integer> leftCopy = new ArrayList<>(cycle);
+            leftCopy.set(middleIndex,leftMidSum);
+            leftCopy.remove(middleIndex - 1);
+            cycles.add(leftCopy);
+            cycleGenerator(leftCopy);
+        }
+
+        if (middleIndex + 1 < cycle.size() - 1) {
+            int rightMidSum = middleValue + cycle.get(middleIndex + 1);
+            ArrayList<Integer> rightCopy = new ArrayList<>(cycle);
+            rightCopy.set(middleIndex,rightMidSum);
+            rightCopy.remove(middleIndex + 1);
+            cycles.add(rightCopy);
+            cycleGenerator(rightCopy);
+        }
     }
 }
