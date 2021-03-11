8e70ef6d3a96a4151d9f06179f66eb321d50d7e2
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-03-10 18:31:07 -0600

    Most work done except for most crucial part

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,107 @@
-public class Main {
-    public static void main(String[] args) {
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class Main {
+    /** finds the maximum order possible of an element from the symmetric group S sub n
+     * @author Nathan Cheshire
+     * @since 3-10-21
+     */
+    public static void main(String[] args) {
+        GOO(20); //should be 420 I believe
+        GOO(40); //no idea personally
+        GOO(7); //12
+        GOO(13); //60
     }
 
-    private static void GOO(int groupSize) {
+    private static void GOO(int n) {
         int max = 0;
-        int[] using = {};
+        ArrayList<Integer> using = null;
+
+        ArrayList<Integer> productOfCycleLengths = null; //the product of the cycle lengths below
+        ArrayList<ArrayList<Integer>> cycles = null; //cycles of which all add up to n
+
         long start = System.currentTimeMillis();
 
-        //todo all
-        //generate list containing 1 to groupSize
-        //find all combos that add up to group size from this list
-        //multiply the combos together
-        //find the max combo and the corresponding sizes
-        //output results
+        ArrayList<Integer> oneToNum = null;
+        for (int i = 1 ; i < n ; i++)
+            oneToNum.add(i);
+
+        //find all combos that add up to group size from this list that
+        // only have relatively prime numbers
+        //add these to correspondingSizes
+
+        //soooo pseudocode:
+        //we go through all groups of size 1, then 2 then 3.... all the way to size n
+
+        for (ArrayList<Integer> cycle : cycles) {
+            productOfCycleLengths.add(LCM(cycle));
+        }
+
+        for (int i = 0 ; i < productOfCycleLengths.size() ;i++) {
+            if (productOfCycleLengths.get(i) > max) {
+                max = productOfCycleLengths.get(i);
+                using = cycles.get(i);
+            }
+        }
 
         long end = System.currentTimeMillis();
-
         System.out.println("Found result in " + (end - start) + " ms.");
-        System.out.println("Result: " + max);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0 ; i < using.size() ; i++)
+            if (i != using.size() - 1)
+                sb.append(using.get(i)).append(",");
+            else
+                sb.append(using.get(i));
+
+        System.out.println("Results:\nmax order: " + max + "\nusing: [" + sb.toString() + "]");
+    }
+
+    boolean relativelyPrime(int a, int b) {
+        return BigInteger.valueOf(a).gcd(BigInteger.valueOf(b)).equals(BigInteger.ONE);
+    }
+
+    private static int[] convertIntegers(List<Integer> integers) {
+        int[] ret = new int[integers.size()];
+        Iterator<Integer> iterator = integers.iterator();
+
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = iterator.next();
+        }
+        return ret;
+    }
+
+    private static int LCM(ArrayList<Integer> arr) {
+        int min;
+        int max;
+        int x;
+        int lcm = 0;
+        int[] myArray = convertIntegers(arr);
+
+        for (int i = 0 ; i < myArray.length ; i++) {
+            for (int j = i + 1; j < myArray.length - 1 ; j++) {
+                if (myArray[i] > myArray[j]) {
+                    min = myArray[j];
+                    max = myArray[i];
+                }
+
+                else {
+                    min = myArray[i];
+                    max = myArray[j];
+                }
+
+                for (int k = 0 ; k < myArray.length ; k++) {
+                    x = k * max;
+
+                    if (x % min == 0) {
+                        lcm = x;
+                    }
+                }
+            }
+        }
+        return lcm;
     }
 }
